d63ab2bb3c48404697bc9587cd09aad9128eb9d0
kritan <kritanshrestha560@gmail.com>
2025-02-19 16:00:13 +0545

    add comments for Button component and its props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
+/**
+ * Interface defining the props for the Button component.
+ */
 type ButtonProps = {
+  /** The text to display on the button. */
   name: string;
+  /** Callback function to be called when the button is clicked. */
   onClick: () => void;
 };
 
+/**
+ * A reusable button component with customizable text and click handler.
+ *
+ * @param {ButtonProps} props - The component's props.
+ * @param {string} props.name - The text to display on the button.
+ * @param {function} props.onClick - Callback function for handling button clicks.
+ *
+ * @example
+ * <Button name="Submit" onClick={() => console.log("Button clicked")} />
+ */
 const Button = (props: ButtonProps) => {
   const { name, onClick } = props;
   return (
