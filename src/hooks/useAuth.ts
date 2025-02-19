686d190774e42ca83249982a005dafee356d0c48
kritan <kritanshrestha560@gmail.com>
2025-02-19 15:40:50 +0545

    add comment on useAuth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,7 @@
+/**
+ * This hook will check for the isAuth key in localStorage and return true if key is found else false
+ * @returns boolean
+ */
 export const useAuth = () => {
   const isAuth = localStorage.getItem("isAuth");
 
